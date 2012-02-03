6b2e7777da9895a370fd5c98cde98d0d8efc5f5b
wush978 <wush978@gmail.com>
2012-02-03 21:17:11 +0800

    add some useful packages

--- packages.R
+++ packages.R
@@ -3,6 +3,15 @@ packages <- c(
   "lattice",
   "inline",
   "sm",
-  "log4r"  
+  "log4r",
+  "mvtnorm",
+  "RgoogleMaps",
+  "mail",
+  "map",
+  "rjson",
+  "HiddenMarkov",
+  "R.oo",
+  "stringr"
   )
 sapply(packages, function(a) install.packages(a))
+
